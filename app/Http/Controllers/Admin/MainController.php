ef08fa1d89cb5122f7432263471c0bb286679c7d
teagjohn <teagjohnbp@gmail.com>
2024-04-11 03:20:22 +0700

    create login screen and main screen

---
+++ app/Http/Controllers/Admin/MainController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+
+class MainController extends Controller
+{
+    //
+    function index()
+    {
+        echo 'Admin';
+    }
+}
